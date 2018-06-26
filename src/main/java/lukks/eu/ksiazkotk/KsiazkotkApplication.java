51b3c09d55fb3c5db903b04eab5ff187fe8e3022
lukasz.dela <lukasz.dela@interia.pl>
2018-06-26 18:59:00 +0200

    project created

---
+++ src/main/java/lukks/eu/ksiazkotk/KsiazkotkApplication.java
@@ -0,0 +1,12 @@
+package lukks.eu.ksiazkotk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiazkotkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsiazkotkApplication.class, args);
+	}
+}
---
+++ src/test/java/lukks/eu/ksiazkotk/KsiazkotkApplicationTests.java
@@ -0,0 +1,16 @@
+package lukks.eu.ksiazkotk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KsiazkotkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
